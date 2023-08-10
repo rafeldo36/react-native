6e7e5930c270c8058533c1788688c96aaf9a326d
Rafey <rafeldo36@gmail.com>
2023-08-10 17:32:26 +0530

    fonts added

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { React, useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  FlatList
+} from 'react-native';
 
-import styles from './welcome.style'
+import { useRouter } from 'expo-router';
+
+import styles from './welcome.style';
+import { icons, SIZES } from '../../../constants';
 
 const Welcome = () => {
+  const router = useRouter();
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}> Hello Rafey </Text>
+        <Text style={styles.welcomeMessage}> Find your perfect job </Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+          style={styles.searchInput}
+          />
+        </View>
+      </View>
     </View>
   )
 }
